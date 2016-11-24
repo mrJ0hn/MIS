f74fed2e591ff6b465beecfce04a12e674094ce0
se <no>
2016-11-24 19:36:51 +0300

    Create SpecializationPage and connect it in frameControl

--- MIS/Pages/SpecializationPage.xaml.cs
+++ MIS/Pages/SpecializationPage.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MIS
+namespace MIS.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для AddSpecialization.xaml
+    /// Логика взаимодействия для SpecializationPage.xaml
     /// </summary>
-    public partial class AddSpecialization : Window
+    public partial class SpecializationPage : Page
     {
-        public AddSpecialization()
+        public SpecializationPage()
         {
             InitializeComponent();
         }
