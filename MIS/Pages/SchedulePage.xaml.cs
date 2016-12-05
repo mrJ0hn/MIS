c4a01556aa122cd2d5628b8c09066d92ddc88432
se <no>
2016-12-05 12:34:15 +0300

    Created new Calendar

--- MIS/Pages/SchedulePage.xaml.cs
+++ MIS/Pages/SchedulePage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MIS.Model;
+using OutlookCalendar;
+using OutlookCalendar.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +23,7 @@ namespace MIS.Pages
     /// </summary>
     public partial class SchedulePage : Page
     {
+        private Appointments appointments = new Appointments(DateTime.Now);
         public SchedulePage()
         {
             InitializeComponent();
@@ -60,10 +63,26 @@ namespace MIS.Pages
             visitors.Add(visitor);
             visitors.Add(visitor);
             listEmployees.ItemsSource = visitors;
+            DataContext = appointments;
         }
         private void btnNewVisitor_Click(object sender, RoutedEventArgs e)
         {
             new AddClientWindow().Show();
+        }
+
+        private void Calendar_AddAppointment(object sender, RoutedEventArgs e)
+        {
+            Appointment appointment = new Appointment();
+            appointment.Subject = "Subject?";
+            appointment.StartTime = new DateTime(2008, 10, 22, 16, 00, 00);
+            appointment.EndTime = new DateTime(2008, 10, 22, 17, 00, 00);
+
+            AddAppointmentWindow aaw = new AddAppointmentWindow();
+            aaw.DataContext = appointment;
+            aaw.ShowDialog();
+
+            appointments.Add(appointment);
+
         }
     }
 }
---
+++ OutlookCalendar/OutlookCalendar/OutlookCalendar/Model/Appointments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace OutlookCalendar.Model
+{
+    public class Appointments : ObservableCollection<Appointment>
+    {
+        public Appointments(DateTime curDate)
+        {
+            Add(new Appointment() { Subject = "Dummy Appointment #1",
+                StartTime = new DateTime(
+                    curDate.Year, 
+                    curDate.Month,
+                    curDate.Day, 
+                    12, 00, 00),
+                EndTime = new DateTime(
+                    curDate.Year,
+                    curDate.Month,
+                    curDate.Day, 
+                    14, 00, 00) });
+        }
+    }
+}
